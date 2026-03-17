09e2cbfa7d137ab82e94f19ae8e52f25da5d89fa
pradeepboya41-creator <pradeepboya41@gmail.com>
2026-03-17 11:33:17 +0530

    Delete models directory

--- models/fertilizer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const FertilizerSchema = new mongoose.Schema({
-  crop: String,
-  fertilizer: String,
-  description: String
-});
-
-module.exports = mongoose.model("Fertilizer", FertilizerSchema);
