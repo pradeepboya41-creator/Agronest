8eeccbeb91b8f513ea6e8b5d088e40cdabbcc196
Pradeep Boya <pradeepboya41@gmail.com>
2026-03-20 23:08:35 +0530

    removed admin + fixed deploy

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const Fertilizer = require("./models/fertilizer");
-const Admin = require("./models/admin");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/agronest")
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB Connected"))
 .catch(err=>console.log(err));
 
@@ -30,26 +28,8 @@ const fertilizers = await Fertilizer.find();
 res.json(fertilizers);
 });
 
-app.post("/admin/create", async (req,res)=>{
-const admin = new Admin(req.body);
-await admin.save();
-res.json({message:"Admin created"});
-});
+const PORT = process.env.PORT || 5000;
 
-app.post("/admin/login", async (req,res)=>{
-
-const {username,password} = req.body;
-
-const admin = await Admin.findOne({username,password});
-
-if(admin){
-res.json({message:"Login success"});
-}else{
-res.json({message:"Invalid credentials"});
-}
-
-});
-
-app.listen(5000, ()=>{
-console.log("Server running on port 5000");
+app.listen(PORT, ()=>{
+console.log("Server running on port " + PORT);
 });
